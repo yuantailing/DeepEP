0ec4e3829f051d5376ddb18951bb46761806c248
Seth Howell <sethh@nvidia.com>
2025-07-14 22:47:38 -0700

    buffer.py: Do not force the NIC handler to GPU.
    This enables the CPU-Assisted data path.
    
    Signed-off-by: Seth Howell <sethh@nvidia.com>

--- deep_ep/buffer.py
+++ deep_ep/buffer.py
@@ -89,7 +89,6 @@ class Buffer:
             assert num_qps_per_rank > 0
             os.environ['NVSHMEM_DISABLE_P2P'] = '0' if allow_nvlink_for_low_latency_mode else '1'
             os.environ['NVSHMEM_IB_ENABLE_IBGDA'] = '1'
-            os.environ['NVSHMEM_IBGDA_NIC_HANDLER'] = 'gpu'
             os.environ['NVSHMEM_IBGDA_NUM_RC_PER_PE'] = f'{num_qps_per_rank}'
 
             # Make sure QP depth is always larger than the number of on-flight WRs, so that we can skip WQ slot check
